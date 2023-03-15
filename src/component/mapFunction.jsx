35c3986cd00b219e52e0beae0802cbdfd02408b5
Bhavika Verma <bhavika.verma@growthnatives.com>
2023-03-15 18:11:12 +0530

    styling added

--- src/component/mapFunction.jsx
+++ src/component/mapFunction.jsx
@@ -2,7 +2,7 @@ import { React, useEffect, useState } from 'react';
 export function MapFunction () {
     const [getData, setGetData] = useState();
     const fetchData = () =>{
-        return fetch('https://random-data-api.com/api/v2/users?size=2&is_xml=true')
+        return fetch('https://random-data-api.com/api/v2/users?size=5&is_xml=true')
             .then((response) => response.json())
             .then((data) => {
                 setGetData(data)
@@ -20,9 +20,21 @@ export function MapFunction () {
                     <h1>Data from Api {index + 1}</h1>
                     <img src={item.avatar}/>
                     <h3>Personal Information</h3>
-                    <p>Usename: {item.username}, First Name: {item.first_name}, Last Name: {item.last_name}, Gender: {item.gender}, Phone Number: {item.phone_number}</p>
+                    <ul>
+                        <li>Usename: {item.username}</li>
+                        <li>First Name: {item.first_name}</li>
+                        <li>Last Name: {item.last_name}</li>
+                        <li> Gender: {item.gender}</li>
+                        <li>Phone Number: {item.phone_number}</li>
+                    </ul>
                     <h3>Address:</h3>
-                    <p>Street: {item.address.street_address} City: {item.address.city}, State: {item.address.state}, Country {item.address.country}, Zip Code: {item.address.zip_code}</p>
+                    <ul>
+                        <li>Street: {item.address.street_address}</li>
+                        <li>City: {item.address.city}</li>
+                        <li>State: {item.address.state}</li>
+                        <li>Country {item.address.country}</li>
+                        <li>Zip Code: {item.address.zip_code}</li>
+                    </ul>
                 </div> 
             </>
         ))
