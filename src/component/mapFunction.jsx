e2e949f0fa6d05bc4abb4c16de943a21d8bcd48b
Bhavika Verma <bhavika.verma@growthnatives.com>
2023-03-16 15:14:03 +0530

    feedback points fixed

--- src/component/mapFunction.jsx
+++ src/component/mapFunction.jsx
@@ -18,22 +18,22 @@ export function MapFunction () {
             <>
                 <div className="data" key={index}>
                     <h1>Data from Api {index + 1}</h1>
-                    <img src={item.avatar}/>
+                    <img src={item?.avatar}/>
                     <h3>Personal Information</h3>
                     <ul>
-                        <li>Usename: {item.username}</li>
-                        <li>First Name: {item.first_name}</li>
-                        <li>Last Name: {item.last_name}</li>
-                        <li> Gender: {item.gender}</li>
-                        <li>Phone Number: {item.phone_number}</li>
+                        <li>Usename: {item?.username}</li>
+                        <li>First Name: {item?.first_name}</li>
+                        <li>Last Name: {item?.last_name}</li>
+                        <li> Gender: {item?.gender}</li>
+                        <li>Phone Number: {item?.phone_number}</li>
                     </ul>
                     <h3>Address:</h3>
                     <ul>
-                        <li>Street: {item.address.street_address}</li>
-                        <li>City: {item.address.city}</li>
-                        <li>State: {item.address.state}</li>
-                        <li>Country {item.address.country}</li>
-                        <li>Zip Code: {item.address.zip_code}</li>
+                        <li>Street: {item?.address?.street_address}</li>
+                        <li>City: {item?.address?.city}</li>
+                        <li>State: {item?.address?.state}</li>
+                        <li>Country {item?.address?.country}</li>
+                        <li>Zip Code: {item?.address?.zip_code}</li>
                     </ul>
                 </div> 
             </>
