16a837a866df24590bc9037d1ec509ba897e64f5
Bhavika Verma <bhavika.verma@growthnatives.com>
2023-03-16 12:17:26 +0530

    adding slick slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import { MapFunction } from './component/mapFunction';
+import { SlickSlider } from './component/SlickSlider';
 
 function App() {
   return (
     <div className="App">
       <MapFunction/>
+      <SlickSlider/>
     </div>
   );
 }
